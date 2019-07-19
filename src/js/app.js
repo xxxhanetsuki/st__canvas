65495ea9bdb14f1e5ab9d9342d8aa3d340e90e8b
立崎光一 <koichi.tachizaki@person-link.co.jp>
2019-07-19 09:57:11 +0900

    動くところまでは作成（バグあり）

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import {createShape} from './shape'
 
+const movable = 1010;
+
 let width;
 let height;
 const ele = document.getElementById('canvas');
-ele.setAttribute('width', 1010);
-ele.setAttribute('height', height);
 const ctx = ele.getContext('2d');
 
 window.addEventListener('resize', function() {
@@ -18,23 +18,23 @@ function sizing() {
   ele.width = width;
   height = document.getElementById('wrap').clientHeight;
   ele.height = height;
-  create();
-}
 
-function create() {
-  // top
-  createShape(ctx, 145, 0, 13, 'red', 'bm');
-  createShape(ctx, 105, 30, 0, 'blue', 'bm');
-  createShape(ctx, 100, 55, 55, 'red', 'tp');
-  createShape(ctx, 140, 150, 125, 'blue', 'bm');
-  createShape(ctx, 155, 240, 0, 'red', 'bm');
-  createShape(ctx, 60, 770, 0, 'red', 'bm');
-  createShape(ctx, 100, 950, 50, 'blue', 'bm');
+  createShape(ctx, 145, 0, 13, 'red', 'bm', movable, true);
+  createShape(ctx, 105, 30, 0, 'blue', 'bm', movable);
+  createShape(ctx, 100, 55, 55, 'red', 'tp', movable);
+  createShape(ctx, 140, 150, 125, 'blue', 'bm', movable);
+  createShape(ctx, 155, 240, 0, 'red', 'bm', movable);
+  createShape(ctx, 60, 770, 0, 'red', 'bm', movable);
+  createShape(ctx, 100, 950, 50, 'blue', 'bm', movable);
 
   // under
-  createShape(ctx, 160, 10, (height-160), 'blue', 'tp');
-  createShape(ctx, 130, 80, (height-130-55), 'red', 'tp');
-  createShape(ctx, 190, 130, (height-80), 'red', 'tp');
-  createShape(ctx, 190, 260, (height-190-35), 'blue', 'bm');
-  createShape(ctx, 60, 700, (height-60-10), 'red', 'tp');
+  createShape(ctx, 160, 10, (height-160), 'blue', 'tp', movable);
+  createShape(ctx, 130, 80, (height-130-55), 'red', 'tp', movable);
+  createShape(ctx, 190, 130, (height-80), 'red', 'tp', movable);
+  createShape(ctx, 190, 260, (height-190-35), 'blue', 'bm', movable);
+  createShape(ctx, 60, 700, (height-60-10), 'red', 'tp', movable);
+}
+
+export const clearRect = () => {
+  ctx.clearRect(0, 0, width, height); // 初期化
 }
--- src/js/shape.js
+++ src/js/shape.js
@@ -1,20 +1,48 @@
+import { clearRect } from './app';
+
 const colorPalet = {
   red: '#ef2b7e40',
   blue: '#00a2d640'
 }
 
-export const createShape = (ctx, size, posWidth, posHight, color, type) => {
-  ctx.beginPath();
-  ctx.moveTo(posWidth, posHight);
-  if (type === 'bm') {
-    ctx.lineTo(size + posWidth, posHight);
-    ctx.lineTo(posWidth, size + posHight);
-  }
-  else {
-    ctx.lineTo(size + posWidth, size + posHight);
-    ctx.lineTo(posWidth, size + posHight);
-  }
-  ctx.closePath();
-  ctx.fillStyle = colorPalet[color];
-  ctx.fill();
+export const createShape = (ctx, size, initialWidth, posHight, color, type, movable, isClear) => {
+  let count = movable
+
+  const interval = setInterval(() => {
+
+    if (isClear) clearRect();
+    const posWidth = formula(initialWidth, count); // 二次関数
+
+    // 描画
+    ctx.beginPath();
+    ctx.moveTo(posWidth, posHight);
+    if (type === 'bm') {
+      ctx.lineTo(size + posWidth, posHight);
+      ctx.lineTo(posWidth, size + posHight);
+    }
+    else {
+      ctx.lineTo(size + posWidth, size + posHight);
+      ctx.lineTo(posWidth, size + posHight);
+    }
+    ctx.closePath();
+    ctx.fillStyle = colorPalet[color];
+    ctx.fill();
+    // 描画
+
+    count = count - 1
+
+    if (count <= 0) {
+      clearInterval(interval)
+    }
+  }, 2)
+
+
+
 };
+
+
+
+
+function formula(initial, movable) {
+  return initial + ( 0.001 * Math.pow(movable, 2))
+}
